21e68651a9fa5973dcbe90124808f90161fe6965
amalshehu <amalshehu@gmail.com>
2021-06-15 14:11:04 +0530

    Modify program input

--- src/entrypoint.rs
+++ src/entrypoint.rs
@@ -1,21 +1,14 @@
-// Program entry point
-
 use solana_program::{
-    account_info::AccountInfo, entrypoint, entrypoint::ProgramResult, msg, pubkey::Pubkey,
+    account_info::AccountInfo, entrypoint, entrypoint::ProgramResult, pubkey::Pubkey,
 };
 
-entrypoint!(process_instruction);
+use crate::processor::Processor;
 
+entrypoint!(process_instruction);
 fn process_instruction(
     program_id: &Pubkey,
     accounts: &[AccountInfo],
     instruction_data: &[u8],
 ) -> ProgramResult {
-    msg!(
-        "process_instruction: {}: {} accounts, data={:?}",
-        program_id,
-        accounts.len(),
-        instruction_data
-    );
-    Ok(())
+    Processor::process(program_id, accounts, instruction_data)
 }
